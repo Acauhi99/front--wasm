12adf58320bd68cd6f24d18c0905b2dcea96a29a
msarlo <mateussarlo1@outlook.com>
2025-04-18 18:50:57 -0300

    feat: criar model de GoldPackage

---
+++ Models/GoldPackage.cs
@@ -0,0 +1,9 @@
+namespace front__wasm.Models
+{
+    public class GoldPackage
+    {
+        public int GoldAmount { get; set; }
+        public double Price { get; set; }
+        public bool IsPopular { get; set; }
+    }
+}
