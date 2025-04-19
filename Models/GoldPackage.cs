b7b2f81d26710bfe606369c36c60f91fed723879
msarlo <mateussarlo1@outlook.com>
2025-04-19 16:18:48 -0300

    fix: renderizar o componente goldpackage

--- Models/GoldPackage.cs
+++ Models/GoldPackage.cs
@@ -2,6 +2,7 @@ namespace front__wasm.Models
 {
     public class GoldPackage
     {
+        public int Id { get; set; }
         public int GoldAmount { get; set; }
         public double Price { get; set; }
         public bool IsPopular { get; set; }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<CatalogService>();
+builder.Services.AddScoped<GoldService>();
 
 // Add logging configuration
 builder.Logging.SetMinimumLevel(LogLevel.Debug);
